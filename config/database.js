783248cf8360e087225cf966b9aa8966e5ded17f
Milind <milind@amarinfotech.com>
2019-12-24 18:21:45 +0530

    mean auth app with mysql

---
+++ config/database.js
@@ -0,0 +1,6 @@
+module.exports = {
+  // configure the code below with your username, password and mlab database information
+  database: 'mongodb://meanauthappdb:meanauthappdb0@ds135547.mlab.com:35547/meanauthappdb',   //prod
+  //database: 'mongodb://localhost:27017/meanauth',    //dev
+  secret: 'yoursecret'
+}
